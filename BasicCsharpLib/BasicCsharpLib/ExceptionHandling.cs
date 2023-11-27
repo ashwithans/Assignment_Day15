9a632ae3acd3ccc26da018f42c1416704f2754a8
ashwithans <ashwithans@gmail.com>
2023-11-27 13:11:28 +0530

    Assignment_day15

---
+++ BasicCsharpLib/BasicCsharpLib/ExceptionHandling.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCsharpLib
+{
+    public class ExceptionHandling
+    {
+        public void EnterAge()
+        {
+            do
+            {
+                Console.WriteLine("Enter yor age to check your are minor/adult/senior");
+                int age = int.Parse(Console.ReadLine());
+                try
+                {
+                    if (age > 0 && age < 18)
+                    {
+                        Console.WriteLine("Person is minor");
+                    }
+                    else if (age >= 18 && age < 65)
+                    {
+                        Console.WriteLine("Person is adult");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Person is senior");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error Occured" + e.Message);
+                }
+            }
+            while (false);
+
+        }
+
+
+    }
+}
